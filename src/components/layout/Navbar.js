364aa9a62a8826b3ad6597b0dfa48149d4a16fbd
Brian Bower <brian@brianbower.com>
2020-03-12 11:02:16 -0600

    Add navbar component

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export class Navbar extends Component {
+  render() {
+    return (
+      <nav className="bg-primary">
+        <h1>
+          <i className="fab fa-github"></i> Navbar
+        </h1>
+      </nav>
+    )
+  }
+}
+
+export default Navbar
