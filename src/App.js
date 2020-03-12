e1b9da61b5e2fa55d36c5937c75047ee461ba6b8
Brian Bower <brian@brianbower.com>
2020-03-11 21:40:34 -0600

    Add labelFor

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
+import React, { Fragment, Component } from 'react';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="app">
         <h1>Hello from React!</h1>
+        <label htmlFor="Name">Name</label>
       </div>
-    );    
+    );
   }
 }
 
