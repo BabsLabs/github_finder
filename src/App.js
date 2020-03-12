508a39113755355b3921d8f34154bc0d46991e8d
Brian Bower <brian@brianbower.com>
2020-03-11 21:41:43 -0600

    Change fragment to empty carrot brackets

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <Fragment>
+      <>
         <h1>Hello from React!</h1>
         <label htmlFor="Name">Name</label>
-      </Fragment>
+      </>
     );
   }
 }
