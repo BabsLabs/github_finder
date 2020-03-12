b2df831156bfb8839b75fcd619a072ad511d5f54
Brian Bower <brian@brianbower.com>
2020-03-11 21:47:22 -0600

    Add conditional for loading

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React, { Fragment, Component } from 'react';
 import './App.css';
 
 class App extends Component {
-foo = () => "Tony Lemons";
-
   render() {
     const name = "Tony Lemons";
+    const loading = true;
+    if (loading){
+      return <h4>Loading...</h4>
+    }
     return (
       <div className="App">
         <h1>Hello {this.foo()} from React! </h1>
