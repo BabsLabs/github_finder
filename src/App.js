ec7cef1268350c77590a72da9f2156557eb3c55b
Brian Bower <brian@brianbower.com>
2020-03-12 10:36:17 -0600

    Add double terinary operator

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 class App extends Component {
   render() {
     const name = "Tony Lemons";
-    const loading = true;
+    const loading = false;
+    const showName = false;
+
     return (
       <div className="App">
         <h1>My App!</h1>
-        { loading ? <h4>Loading...</h4> : <h1>Hello {name} from React!</h1> }
+        { loading ? <h4>Loading...</h4> : <h1>Hello {showName ? name : null} from React!</h1> }
       </div>
     );
   }
