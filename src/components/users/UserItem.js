afd3ff813087dde3cea3eec7182ec1d1412b6d19
Brian Bower <brian@brianbower.com>
2020-03-13 13:43:40 -0600

    Remove state and get data from props

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,15 +1,8 @@
 import React, { Component } from 'react'
 
 class UserItem extends Component {
-	state = {
-		id: "id",
-		login: "babslabs",
-		avatar_url: "https://avatars1.githubusercontent.com/u/38663414?v=4",
-		html_url: "https://github.com/BabsLabs"
-	};
-
 	render() {
-    const { login, avatar_url, html_url } = this.state
+    const { login, avatar_url, html_url } = this.props.user
 		return (
 
 			<div className="card text-center">
