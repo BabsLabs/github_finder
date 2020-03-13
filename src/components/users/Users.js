3463b95e687386ddb525390fda44b8d6d4ede189
Brian Bower <brian@brianbower.com>
2020-03-13 13:43:54 -0600

    Add Users component to diplay list of Users

---
+++ src/components/users/Users.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+import UserItem from "./UserItem"
+
+class Users extends Component {
+  state = {
+    users: [
+      {
+        id: 1,
+        login: "babslabs",
+        avatar_url: "https://avatars1.githubusercontent.com/u/38663414?v=4",
+        html_url: "https://github.com/BabsLabs"
+      },
+      {
+        id: 2,
+        login: "mojombo",
+        avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
+        html_url: "https://github.com/mojombo",
+    },
+    {
+        id: 3,
+        login: "defunkt",
+        avatar_url: "https://avatars0.githubusercontent.com/u/2?v=4",
+        html_url: "https://github.com/defunkt",
+    },
+    {
+        id: 4,
+        login: "pjhyett",
+        avatar_url: "https://avatars0.githubusercontent.com/u/3?v=4",
+        html_url: "https://github.com/pjhyett",
+    }
+    ]
+  }
+  
+  render() {
+    return (
+      <div style={userStyle}>
+        {this.state.users.map(user => (
+          < UserItem key={user.id} user={user} />
+        ))}
+      </div>
+    )
+  }
+}
+
+const userStyle = {
+  display: "grid",
+  gridTemplateColumns: "repeat(3, 1fr)",
+  gridGap: "1rem"
+}
+
+export default Users
