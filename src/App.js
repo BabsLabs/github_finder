3c668961204903cdff666ca9f09ab905f3bfc35d
Brian Bower <brian@brianbower.com>
2020-03-16 15:34:30 -0600

    Add alert with timeout if no user is input

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
 import React, { Component } from 'react';
-import Navbar from "./components/layout/Navbar"
+import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
 import Search from "./components/users/Search";
+import Alert from "./components/layout/Alert";
 import axios from 'axios';
 import './App.css';
 
 class App extends Component {
 	state = {
 		users: [],
-		loading: false
+		loading: false,
+		alert: null
 	}
 
 	// Search GitHub users
 	searchUsers = async (text) => {
 		this.setState({ loading: true })
 		
-		const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+		const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
 
-		this.setState({ users: res.data.items, loading: false });
+		this.setState({ users: res.data.items, loading: false })
 	}
 
 	// Clear users from state
 	clearUsers = () => {
 		this.setState({ users: [], loading: false })
+	}
+	
+	setAlert = (message, type) => {
+		this.setState({ alert : { message, type } })
+
+		setTimeout(() => this.setState({ alert: null }), 5000)
 	}
 
   render() {
@@ -32,7 +40,8 @@ class App extends Component {
 			<div className="App">
 				<Navbar />
 				<div className="container">
-					<Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={  users.length > 0 ? true : false }/>
+					<Alert alert={this.state.alert} />
+					<Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={  users.length > 0 ? true : false } setAlert={this.setAlert}/>
 					<Users loading={ loading } users={ users } />
 				</div>
 			</div>
