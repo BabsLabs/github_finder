c9c07572b5deeec14213e96252c9299c832168d9
Brian Bower <brian@brianbower.com>
2020-03-16 15:34:10 -0600

    Add alert if no user is input

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -14,8 +14,12 @@ class Search extends Component {
 
   onSubmit = (event) => {
     event.preventDefault();
-    this.props.searchUsers(this.state.text);
-    this.setState({text: ""});
+    if (this.state.text === "") {
+      this.props.setAlert("Please enter something", "light");
+    } else {
+      this.props.searchUsers(this.state.text);
+      this.setState({text: ""});
+    }
   }
 
   onChange = (event) => {
